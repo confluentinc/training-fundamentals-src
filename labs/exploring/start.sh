92e61a31220da12bd310fdd069036cb913118abb
David Castaneda <david9castaneda@gmail.com>
2026-05-26 19:00:25 -0500

    Update Kafka topic creation and producer image

--- labs/exploring/start.sh
+++ labs/exploring/start.sh
@@ -37,8 +37,9 @@ EOF
 kafka-topics --bootstrap-server kafka:9092 \
     --topic vehicle-positions \
     --create \
+    --if-not-exists \
     --partitions 6 \
-    --replication-factor 1
+    --replication-factor 1 
 
 cat << EOF
 Topic created.
@@ -49,7 +50,7 @@ EOF
 docker container run -d \
     --name producer \
     --net exploring_confluent \
-    cnfltraining/vp-producer:v2
+    cnfltraining/vp-producer-fallback:v2
 
 echo "Producer up and running!"
 
